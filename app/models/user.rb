f445c7b8e9b95ad4ed768b7eb378ea4798045293
prince475 <danielmwai475@gmail.com>
2023-04-01 11:23:18 +0300

    Adding user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_one :user_profile
 
   has_many :comments
   has_many :user_posts, through: :comments
+
+
+  validates :username, {
+    length: { minimum: 5, maximum: 8 },
+    uniqueness: true,
+    presence: true
+}
+
+validates :email, {
+    uniqueness: true,
+    presence: true
+}
+
 
 end
