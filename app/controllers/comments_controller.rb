fc98521209cf4f1f16f62d2c824a07c8a0d624b4
prince475 <danielmwai475@gmail.com>
2023-04-01 18:04:36 +0300

    Updating comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,53 @@
 class CommentsController < ApplicationController
+  before_action :verify_auth
+
+  def create
+    comment = user.comments.create(comment_params)
+    if comment.valid?
+      app_response(status: :created, data: comment)
+    else
+      app_response(
+        status: :unprocessable_entity,
+        data: comment.errors,
+        message: "failed"
+      )
+    end
+  end
+
+  def update
+    comment = user.comments.find(params[:id]).update(comment_params)
+    if comment
+      app_response(data: { info: "updated comment successfully" })
+    else
+      app_response(
+        message: "failed",
+        data: {
+          info: "something went wrong. could not update comment"
+        },
+        status: :unprocessable_entity
+      )
+    end
+  end
+
+  def destroy
+    user.comments.find(params[:id]).destroy
+    app_response(
+      message: "success",
+      data: {
+        info: "deleted comment successfully"
+      },
+      status: 204
+    )
+  end
+
+  def index
+    comment = user.comments.all
+    app_response(message: "success", data: todos)
+  end
+
+  private
+
+  def comment_params
+    params.permit(:description)
+  end
 end
