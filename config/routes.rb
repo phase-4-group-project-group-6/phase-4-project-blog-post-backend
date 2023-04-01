1221bfb4643241c3dd1b51517f11169d607e18ee
prince475 <danielmwai475@gmail.com>
2023-04-01 14:41:00 +0300

    Adding new routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,25 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
 
 
+
+
+  # user
+  post '/users', to: 'users#register'
+  post '/users/login', to: 'users#login'
+  delete '/users/logout', to: 'users#logout'
+  get '/user/login/check', to: 'users#check_login_status'
+
+
+  # verify auth
+  get '/verify', to: 'application#verify_auth'
+
+
+
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post "/signup", to: "users#create"
-  post "/login", to: "sessions#create"
-  get "/me", to: "users#show"
-  delete "/logout", to: "sessions#destroy"
+  # post "/signup", to: "users#create"
+  # post "/login", to: "sessions#create"
+  # get "/me", to: "users#show"
+  # delete "/logout", to: "sessions#destroy"
 end
