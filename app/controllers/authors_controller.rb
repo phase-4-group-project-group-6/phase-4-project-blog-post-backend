c2305c390e943d841f31c3dc3093553331f40a2c
prince475 <danielmwai475@gmail.com>
2023-04-01 17:29:26 +0300

    Adding methods to author and post controllers

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,14 @@
 class AuthorsController < ApplicationController
+ before_action :verify_auth
+
+ def index
+  user.authors.all
+  app_response(message: 'success', data: author)
+ end
+
+private
+
+ def author_params
+   params.permit(:name, :image, :location)
+ end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+
+  before_action :verify_auth
+
+ def index
+  user.posts.all
+  app_response(message: 'success', data: author)
+ end
+
+private
+
+ def post_params
+   params.permit(:name, :image, :location)
+ end
 end
