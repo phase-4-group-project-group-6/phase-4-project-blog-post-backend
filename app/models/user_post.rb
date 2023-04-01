22557258b01c768b80fe721a49c8c4b8d6bf5ff9
prince475 <danielmwai475@gmail.com>
2023-04-01 12:50:00 +0300

    Adding user profile validations

--- app/models/user_post.rb
+++ app/models/user_post.rb
@@ -1,8 +1,19 @@
 class UserPost < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  
+
 
   has_many :comments
   has_many :users, through: :comments
+
+
+  validates :title, {
+    uniqueness: true,
+    presence: true
+  }
+
+  validates :content, {
+    length: { minimum: 10 },
+    presence: true
+  }
 end
