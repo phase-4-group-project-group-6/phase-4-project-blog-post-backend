439162d513fd25774693ca1f5fc55a03b32eb1d3
prince475 <danielmwai475@gmail.com>
2023-04-01 18:37:55 +0300

    Updated user_profiles_controller

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,2 +1,54 @@
 class UserProfilesController < ApplicationController
+
+  before_action :verify_auth
+
+  def create
+    userprofile = user.userprrofiles.create(userprofile_params)
+    if userprofile.valid?
+      app_response(status: :created, data: userprofile)
+    else
+      app_response(
+        status: :unprocessable_entity,
+        data: userprofile.errors,
+        message: "failed"
+      )
+    end
+  end
+
+  def update
+    userprofile = user.userprrofiles.find(params[:id]).update(userprofile_params)
+    if userprofile
+      app_response(data: { info: "updated userprofile successfully" })
+    else
+      app_response(
+        message: "failed",
+        data: {
+          info: "something went wrong. could not update userprofile"
+        },
+        status: :unprocessable_entity
+      )
+    end
+  end
+
+  def destroy
+    user.userprrofiles.find(params[:id]).destroy
+    app_response(
+      message: "success",
+      data: {
+        info: "deleted userprofile successfully"
+      },
+      status: 204
+    )
+  end
+
+  def index
+    userprofile = user.usersprofiles.all
+    app_response(message: "success", data: userprrofiles)
+  end
+
+  private
+
+  def userprofile_params
+    params.permit(:title, :content)
+  end
 end
