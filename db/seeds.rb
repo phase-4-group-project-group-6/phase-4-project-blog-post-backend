4a3f435d65d9c4533a31e6dc2d3c686664d5d240
prince475 <danielmwai475@gmail.com>
2023-04-03 14:54:42 +0300

    Commenting out seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-Author.destroy_all
-Comment.destroy_all
-Post.destroy_all
-Post.destroy_all
-UserPost.destroy_all
-UserProfile.destroy_all
-User.destroy_all
+# Author.destroy_all
+# Comment.destroy_all
+# Post.destroy_all
+# Post.destroy_all
+# UserPost.destroy_all
+# UserProfile.destroy_all
+# User.destroy_all
 
 
-puts "🧚‍♂️ a tale"
+# puts "🧚‍♂️ a tale"
 
-author1 = Author.create(name: "Leeroy Jenkins")
-author2 = Author.create(name: Faker::Name.unique.name, location: Faker::Address.city)
-author3 = Author.create(name: Faker::Name.unique.name, location: Faker::Address.city)
+# author1 = Author.create(name: "Leeroy Jenkins")
+# author2 = Author.create(name: Faker::Name.unique.name, location: Faker::Address.city)
+# author3 = Author.create(name: Faker::Name.unique.name, location: Faker::Address.city)
 
-user1 = User.create(name: Faker::Lorem.word, email: Faker::Internet.email)
-user2 = User.create(name: Faker::Lorem.word, email: Faker::Internet.email)
-user3 = User.create(name: Faker::Lorem.word, email: Faker::Internet.email)
-
-
-UserProfile.create(user_id: user1.id, username: "ljenk", email: "ljenk@aol.com", bio: "a very dated reference")
-
-Post.create(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, author_id: author1.id, user_id: user1.id)
-Post.create(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, author_id: author2.id, user_id: user2.id)
-
-userpost1 = UserPost.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user_id: user1.id, comment_id: comment1.id)
-userpost2 = UserPost.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user_id: user2.id, comment_id: comment2.id)
+# user1 = User.create(name: Faker::Lorem.word, email: Faker::Internet.email)
+# user2 = User.create(name: Faker::Lorem.word, email: Faker::Internet.email)
+# user3 = User.create(name: Faker::Lorem.word, email: Faker::Internet.email)
 
 
-Comment.create(user_id: user1.id, user_post_id: user_post1.id)
-Comment.create(user_id: user.id, user_post_id: used_post2.id)
+# UserProfile.create(user_id: user1.id, username: "ljenk", email: "ljenk@aol.com", bio: "a very dated reference")
 
-puts  "🧚‍♂️ a tale is already Told"
+# Post.create(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, author_id: author1.id, user_id: user1.id)
+# Post.create(title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, author_id: author2.id, user_id: user2.id)
+
+# userpost1 = UserPost.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user_id: user1.id, comment_id: comment1.id)
+# userpost2 = UserPost.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user_id: user2.id, comment_id: comment2.id)
+
+
+# Comment.create(user_id: user1.id, user_post_id: user_post1.id)
+# Comment.create(user_id: user.id, user_post_id: used_post2.id)
+
+# puts  "🧚‍♂️ a tale is already Told"
 
 
 
